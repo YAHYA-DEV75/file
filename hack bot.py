3edd6292c3ff30c66305d792c2b0691073d2efc3
YAHYA-DEV75 <green.boy.rajawi1@gmail.com>
2026-01-11 01:33:30 +0100

    Create hack bot.py

---
+++ hack bot.py
@@ -0,0 +1,30 @@
+print("                                  😈THE MOLY BOT😈")
+
+print("                     I MAKE MISTAKS BUT AT AWAYS COME BACK")
+                                   
+print("           HACK INSTA                                     30.00$💰")
+print("           HACK FACEBOOK                                  10.00$💰")
+print("           HACK WHATSUP                                   40.00$💰")                          
+
+print("                TO CHOOSE INSTA PRESSE             1️⃣")
+print("                TO CHOOSE FACEBOOK PRESSE          2️⃣")
+print("                TO CHOOSE WHATSUP PRESSE           3️⃣")
+
+print("               TO CONTACT THE DEVELOPER ENTER THE PIN :")
+
+
+
+usr = input("to choose!:")
+
+
+if usr == "1":
+   print("coming son")
+
+if usr == "2":
+   print("coming son")
+
+if usr == "3":
+   print("coming son")
+
+if usr == "1234":
+   print("this is telegra me of devloper: twest_12 ")
